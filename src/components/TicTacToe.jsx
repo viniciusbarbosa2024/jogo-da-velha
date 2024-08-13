749dd981926feda83a4de70dade59b94416ee408
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-08-12 21:46:48 -0300

    Melhoria no código

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -2,13 +2,18 @@ import {useState} from 'react'
 import './TicTacToe.css'
 
 export const TicTacToe = () => {
-    const [position,setPosition] = useState(Array(9).fill(''))
+    const [position,setPosition] = useState(Array(9).fill('')) //Anotar sobre o .fill
 
-    function updatePosition(index) {
+    function generalFunction(index) {
         let numberOfPlays = position.filter((element)=> element === 'X' || element==="O").length
 
         let positionCopy = [...position]
 
+        updatePosition(numberOfPlays,positionCopy,index)
+
+    }
+
+    function updatePosition(numberOfPlays,positionCopy,index) {
         if (numberOfPlays%2 == 0) {
             positionCopy[index] = 'X'
             setPosition(positionCopy)
@@ -16,16 +21,15 @@ export const TicTacToe = () => {
             positionCopy[index] = 'O'
             setPosition(positionCopy)
         }
-        
     }
   
     return (
     <main>
         {position.map((element,index)=> {
             return (
-                <div key={index} className={`position ${element}`} onClick={() => updatePosition(index)}>
+                <div key={index} className={`position ${element}`} onClick={() => generalFunction(index)}>
                     
-                </div>
+                </div> //Anotar sobre o key
             )
         })}
     </main>
