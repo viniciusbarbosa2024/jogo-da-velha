a5e46b21a3aa91e95f59fc1f1d497960bb0f0ff6
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-08-13 13:55:51 -0300

    Melhoria do código da checkWinner

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -16,42 +16,53 @@ export const TicTacToe = () => {
         
     }
 
-    function checkWinner(positionCopy){
-       let X_positions = []
-       let O_positions = []
-       for (let pos =0;pos< positionCopy.length;pos++) {
-        if (positionCopy[pos] === 'X') {
-            X_positions.push(pos)
-        } else if (positionCopy[pos] === 'O'){
-            O_positions.push(pos)
+    //Obter array de posições jogadas por dado player
+    function getPositions(positionCopy,player) {
+        let positions = []
+        for (let pos =0;pos< positionCopy.length;pos++) {
+            if (positionCopy[pos] === player) {
+                positions.push(pos)
+            }
         }
-       }
 
-       let positionsToWin = [
-        //Horizontal
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
+        return positions
+    }
 
-        //Vertical
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
+    //Verificar se as posições jogadas retornam um vencedor do jogo
+    function checkPlayedPositions(positionsToWin,X_positions,O_positions) {
+        positionsToWin.forEach((winningStreak) => {
 
-        //Diagonal
-        [0,4,8],
-        [2,4,6]
-       ]
-
-        positionsToWin.forEach((element) => {
-
-            if (element.every((element) => X_positions.indexOf(element) != -1) === true) {
+            if (winningStreak.every((element) => X_positions.indexOf(element) != -1) === true) {
                 return alert('x ganhou')
-            } else if (element.every((element) => O_positions.indexOf(element) != -1) === true) {
+            } else if (winningStreak.every((element) => O_positions.indexOf(element) != -1) === true) {
                 return alert('O ganhou')
             }
 
        })
+    }
+
+    function checkWinner(positionCopy){
+        //Arrays com sequência de posições que posibilitam a vitória
+        let positionsToWin = [
+            //Horizontal
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+    
+            //Vertical
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+    
+            //Diagonal
+            [0,4,8],
+            [2,4,6]
+           ]
+       
+        let X_positions = getPositions(positionCopy,'X')
+        let O_positions = getPositions(positionCopy,'O')
+
+        checkPlayedPositions(positionsToWin,X_positions,O_positions)
        
     }
     
